f810dca2d85674d4b0ecc7229cad876d6336a99d
Preston Smith <smith.preston2@gmail.com>
2020-12-04 07:59:42 -0500

    elements stay centered on resize

--- components/canvas/canvas.two.tsx
+++ components/canvas/canvas.two.tsx
@@ -2,61 +2,69 @@ import React, { useState, useRef, useEffect } from 'react';
 import Two from 'two.js';
 import styles from './canvas.module.scss';
 
+const params = {
+  autostart: true,
+  fullscreen: true,
+}
+
 const canvasTwo: React.FC = () => {
-  let two: Two;
-  const stageRef = useRef<HTMLDivElement>(null);
-  const [shapes, setShapes] = React.useState<Two.Object[]>([]);
+  var stageRef = useRef();
+  var twoRef = useRef<Two>();
 
-  useEffect(() => {
-    setup()
-   }, []);
+  useEffect(setup, []);
 
-  const setup = () => {
+  function setup() {
     const stage = stageRef.current as HTMLDivElement;
-    two = new Two({
-    //   width: stage.clientWidth,
-    //   height: stage.clientHeight,
-      autostart: true,
-      fullscreen: true,
-    })
-    
+    twoRef.current = new Two(params);
+
     if(stage.children.length < 1)
-      two.appendTo(stage);
+      twoRef.current.appendTo(stageRef.current);
 
     // Add any shapes you'd like here
-    const twoShapes: Two.Object[] = [];
-    const shape = two.makeCircle(500, 500, 250);
-    
-    twoShapes.push(shape);
-    setShapes(twoShapes)
-    two.add(shapes)
-    
-    two.bind('update', update);
-    two.bind('resize', resize);
-    two.update();
+    var circle = twoRef.current.makeCircle(-70, 0, 50);
+    var rect = twoRef.current.makeRectangle(70, 0, 100, 100);
+    circle.fill = '#FF8000';
+    rect.fill = 'rgba(0, 200, 255, 0.75)';
 
+    var group = twoRef.current.makeGroup(circle, rect);
+    group.translation.set(twoRef.current.width / 2, twoRef.current.height / 2);
+    group.scale = 0;
+    group.noStroke();
+
+    twoRef.current.bind('update', () => update(group));
+    twoRef.current.bind('resize', () => resize(group));
+
+    return function() {
+      // Unmount handler
+      twoRef.current.unbind('update', update);
+      twoRef.current.unbind('resize', resize);
+    }
   }
 
-  const resize = () => {
+  function resize(group) {
     // Resize logic here
     console.log('resizing!');
     const stage = stageRef.current as HTMLElement;
+    const two = twoRef.current as Two;
     
     console.log(`Stage is: ${stage.clientWidth} by ${stage.clientHeight}`);
-    console.log(`Two is: ${two?.width} by ${two.height}`);
-    
-    two.width = stage.clientWidth || two.width ;
-    two.height = stage.clientHeight || two.height;
-
-    // two.update();
+    console.log(`Two is: ${two.width} by ${two.height}`);
+    group.translation.set(twoRef.current.width / 2, twoRef.current.height / 2);
   }
 
-  function update() {
+  function update(group: Two.Group) {
     // animate shapes here
+    // console.log('updating');
+    if (group.scale > 0.9999) {
+      group.scale = group.rotation = 0;
+    }
+    var t = (1 - group.scale) * 0.125;
+    group.scale += t;
+    group.rotation += t * 4 * Math.PI;
   }
 
   return (
-    <div className={`${styles.stage} stage`} ref={ stageRef } />
+    <div className="stage" ref={ stageRef } />
   );
 }
 
