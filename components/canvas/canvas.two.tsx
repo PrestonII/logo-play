fe2b7894a5f2a9d3fc018ae43d560b7251fe2d6e
Preston Smith <smith.preston2@gmail.com>
2020-12-05 10:50:23 -0500

    makes a pixel grid

--- components/canvas/canvas.two.tsx
+++ components/canvas/canvas.two.tsx
@@ -10,7 +10,7 @@ const params = {
 const canvasTwo: React.FC = () => {
   var stageRef = useRef();
   var twoRef = useRef<Two>();
-  const [stateGroup, setGroup] = React.useState<Two.Group>();
+  // const [stateGroup, setGroup] = React.useState<Two.Group>();
 
   useEffect(setup, []);
 
@@ -24,20 +24,23 @@ const canvasTwo: React.FC = () => {
 
     // Add any shapes you'd like here
     // createShapes(two);
-    var circle = two.makeCircle(-70, 0, 50);
-    var rect = two.makeRectangle(70, 0, 100, 100);
-    circle.fill = '#FF8000';
-    circle.stroke = 'orangered';
-    rect.fill = 'rgba(0, 200, 255, 0.75)';
-    rect.stroke = '#1C75BC';
+    // var circle = two.makeCircle(-70, 0, 50);
+    // var rect = two.makeRectangle(70, 0, 100, 100);
+    // circle.fill = '#FF8000';
+    // circle.stroke = 'orangered';
+    // rect.fill = 'rgba(0, 200, 255, 0.75)';
+    // rect.stroke = '#1C75BC';
 
-    var group = two.makeGroup(circle, rect);
-    // two.add(group);
-    group.translation.set(two.width / 2, two.height / 2);
-    group.scale = 1;
-    // group.noStroke();
-    group.linewidth = 7;
-    setGroup(group);
+    // var group = two.makeGroup(circle, rect);
+    // // two.add(group);
+    // group.translation.set(two.width / 2, two.height / 2);
+    // group.scale = 1;
+    // // group.noStroke();
+    // group.linewidth = 7;
+    // // setGroup(group);
+    // const rects = makeRowOfPixels(two, two.width, 0, 40);
+    const rects = makePixelGrid(two, two.width, two.height, 40);
+    const group = two.makeGroup(rects);
 
     two.bind('update', () => update(group));
     two.bind('resize', () => resize(group));
@@ -51,22 +54,46 @@ const canvasTwo: React.FC = () => {
     }
   }
 
-  function createShapes(two: Two) {
-    var circle = two.makeCircle(-70, 0, 50);
-    var rect = two.makeRectangle(70, 0, 100, 100);
-    circle.fill = '#FF8000';
-    circle.stroke = 'orangered';
-    rect.fill = 'rgba(0, 200, 255, 0.75)';
-    rect.stroke = '#1C75BC';
+  function makePixelGrid(two: Two, width: number, height: number, size: number): Two.Rectangle[] {
+    const grid: Two.Rectangle[] = [];
 
-    var group = two.makeGroup(circle, rect);
-    // two.add(group);
-    group.translation.set(two.width / 2, two.height / 2);
-    group.scale = 1;
-    // group.noStroke();
-    group.linewidth = 7;
-    setGroup(group);
+    for(let index = 0; index < height; index += size) {
+      const row = makeRowOfPixels(two, width, index, size);
+      grid.push(...row);
+    }
+
+    return grid;
   }
+
+  function makeRowOfPixels(two: Two, width: number, position: number, size: number): Two.Rectangle[] {
+    const rectangles: Two.Rectangle[] = [];
+
+    for(let index = 0; index < width; index += size) {
+      const rect = two.makeRectangle(index, position, size, size);
+      rect.fill = '#1c75bc';
+      // rect.noStroke();
+      rectangles.push(rect)
+    }
+
+    return rectangles;
+  }
+
+  // function createShapes(two: Two) {
+    // var circle = two.makeCircle(-70, 0, 50);
+    // var rect = two.makeRectangle(70, 0, 100, 100);
+    // circle.fill = '#FF8000';
+    // circle.stroke = 'orangered';
+    // rect.fill = 'rgba(0, 200, 255, 0.75)';
+    // rect.stroke = '#1C75BC';
+
+    // var group = two.makeGroup(circle, rect);
+    // // two.add(group);
+    // group.translation.set(two.width / 2, two.height / 2);
+    // group.scale = 1;
+    // // group.noStroke();
+    // group.linewidth = 7;
+    // // setGroup(group);
+  // }
 
   function resize(group) {
     // Resize logic here
@@ -82,12 +109,12 @@ const canvasTwo: React.FC = () => {
   function update(group: Two.Group) {
     // animate shapes here
     // console.log('updating');
-    if (group.scale > 0.9999) {
-      group.scale = group.rotation = 0;
-    }
-    var t = (1 - group.scale) * 0.125;
-    group.scale += t;
-    group.rotation += t * 4 * Math.PI;
+    // if (group.scale > 0.9999) {
+    //   group.scale = group.rotation = 0;
+    // }
+    // var t = (1 - group.scale) * 0.125;
+    // group.scale += t;
+    // group.rotation += t * 4 * Math.PI;
   }
 
   return (
