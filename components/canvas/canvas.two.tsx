d858e335f8641623a71d029035b274bda9573ad7
Preston Smith <smith.preston2@gmail.com>
2020-12-05 12:42:00 -0500

    making loading synchronous

--- components/canvas/canvas.two.tsx
+++ components/canvas/canvas.two.tsx
@@ -10,34 +10,64 @@ const params = {
 const canvasTwo: React.FC = () => {
   var stageRef = useRef();
   var twoRef = useRef<Two>();
-  // const [stateGroup, setGroup] = React.useState<Two.Group>();
+  const [two, setTwo] = React.useState<Two>();
+  const [shapes, setShapes] = React.useState<Element>(null);
+  const [isLoaded, setLoaded] = React.useState(false);
 
   useEffect(setup, []);
+  useEffect(makeStage, [two]);
+  useEffect(confirmLoad, [isLoaded]);
+
+  function confirmLoad() {
+    if(isLoaded)
+      console.log('LOAD COMPLETE!');
+  }
+
+  function makeStage() {
+    if(two)
+      loadStage();
+
+    return function() {
+      console.log('Detaching stage...');
+    }
+  }
 
   function setup() {
+    console.log('loading...');
     twoRef.current = new Two(params);
-    const stage = stageRef.current as HTMLDivElement;
+    
     const two = twoRef.current;
+    setTwo(twoRef.current);
 
+    return function() {
+      // Unmount handler
+      console.log('unmounting...');
+      setLoaded(false);
+      two.unbind('update', update);
+      two.unbind('resize', resize);
+      console.log('UNMOUNT COMPLETE!');
+    }
+  }
+
+  function loadStage() {
+    const stage = stageRef.current as HTMLDivElement;
     if(stage.children.length < 1)
       two.appendTo(stageRef.current);
 
     const grid = makePixelGrid(two, two.width, two.height, 40);
     console.log(grid)
     const group = two.makeGroup(grid);
+    
 
-    const rects = document.querySelector('.stage > svg > g > g').children;
-    console.log(rects.length);
-    console.log(rects);
-
-    two.bind('update', () => update(group));
-    two.bind('resize', () => resize(group));
-
-    return function() {
-      // Unmount handler
-      two.unbind('update', update);
-      two.unbind('resize', resize);
-    }
+    // const rects = document.querySelector('.stage > svg > g > g');
+    // const limit = rects.length;
+    // const ranNums = Array.from(Array(limit).keys());
+    // console.log(ranNums);
+    // console.log(rects.length);
+    // console.log(rects);
+    two.bind(Two.Events.update.toString(), update);
+    two.bind(Two.Events.resize.toString(), resize);
+    setLoaded(true);
   }
 
   function makePixelGrid(two: Two, width: number, height: number, size: number): Two.Rectangle[] {
@@ -83,7 +113,7 @@ const canvasTwo: React.FC = () => {
     // // setGroup(group);
   // }
 
-  function resize(group) {
+  function resize() {
     // Resize logic here
     console.log('resizing!');
     const stage = stageRef.current as HTMLElement;
@@ -91,10 +121,29 @@ const canvasTwo: React.FC = () => {
     
     console.log(`Stage is: ${stage.clientWidth} by ${stage.clientHeight}`);
     console.log(`Two is: ${two.width} by ${two.height}`);
-    group.translation.set(twoRef.current.width / 2, twoRef.current.height / 2);
+    // group.translation.set(twoRef.current.width / 2, twoRef.current.height / 2);
   }
 
-  function update(group: Two.Group) {
+  function update(group?: Two.Group) {
+    // console.log('updating!');
+    // console.log(`The document loaded: ${isLoaded}`);
+    // console.log(`The shapes loaded: ${shapes?.children.length}`);
+
+    // const rects = document.querySelector('.stage > svg > g > g');
+
+    // if(isLoaded && !shapes) {
+    //   setShapes(rects)
+    //   // console.log(rects);
+    // }
+
+    // if(shapes)
+    //   console.log(shapes);
+
+    // // console.log(isLoaded);
+    // if(isLoaded) {
+    //   console.log(shapes.children.length);
+    // }
+    
     // animate shapes here
     // console.log('updating');
     // if (group.scale > 0.9999) {
